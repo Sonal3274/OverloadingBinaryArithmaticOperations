9e3bf302a5bdb91cd43c2daebd7eb06e72c171e5
Sonal Dhage <sonal.dhage@thermondo.de>
2023-02-15 18:55:48 +0100

    Created an example for Equality Operators

---
+++ src/main/kotlin/EqualityOperators.kt
@@ -0,0 +1,23 @@
+fun main() {
+    val x = 5
+    val y = 10
+    val z = 5
+
+    if (x == y) {
+        println("x is equal to y")
+    } else {
+        println("x is not equal to y")
+    }
+
+    if (x == z) {
+        println("x is equal to z")
+    } else {
+        println("x is not equal to z")
+    }
+
+    if (x != y) {
+        println("x is not equal to y")
+    } else {
+        println("x is equal to y")
+    }
+}
